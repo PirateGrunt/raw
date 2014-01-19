377762a0436b9834d9ba0e5d941f13d82e506ce8
BrianFannin <BFannin@RedwoodsGroup.com>
2014-01-19 09:23:06 -0500

    Part 2

--- MakeScript.R
+++ MakeScript.R
@@ -1,4 +1,21 @@
 library(knitr)
 library(tools)
-knit("./Part2/RPM-Part2.Rnw", "./Part2/RPM-Part2.tex")
-texi2pdf("./Part2/RPM-Part2.tex", clean=TRUE, quiet=FALSE)
+
+setwd("V:/My Documents/GitHub/RPM2014/")
+
+# Utilities to make child documents
+BuildChild = function(childDir){
+  require(knitr)
+  require(tools)
+  setwd(paste0("V:/My Documents/GitHub/RPM2014/", childDir, "/"))
+  rnwName = paste0("RPM-", childDir, ".Rnw")
+  texName = paste0("RPM-", childDir, ".tex")
+  knit(rnwName, texName)
+  texi2pdf(texName, clean=TRUE, quiet=FALSE)
+  #unlink(c("*.log", "*.nav", "*.snm", "*.toc", "*.vrb", "*concordance.tex"))
+  unlink(c("*.log", "*.nav", "*.snm", "*.toc", "*.vrb", "*.aux", "*.out"))
+  setwd("V:/My Documents/GitHub/RPM2014/")
+}
+
+BuildChild("Part2x")
+
