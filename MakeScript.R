371a82b65dcac5b685ad668afc76a1428856015d
BrianFannin <BFannin@RedwoodsGroup.com>
2014-02-26 11:57:32 -0500

    Various

--- MakeScript.R
+++ MakeScript.R
@@ -1,21 +1,24 @@
-library(knitr)
-library(tools)
-
-setwd("V:/My Documents/GitHub/RPM2014/")
-
-# Utilities to make child documents
-BuildChild = function(childDir){
+BuildDoc = function(dir, rnwName, texName){
   require(knitr)
   require(tools)
-  setwd(paste0("V:/My Documents/GitHub/RPM2014/", childDir, "/"))
-  rnwName = paste0("RPM-", childDir, ".Rnw")
-  texName = paste0("RPM-", childDir, ".tex")
+  
+  setwd(dir)
   knit(rnwName, texName)
   texi2pdf(texName, clean=TRUE, quiet=FALSE)
-  #unlink(c("*.log", "*.nav", "*.snm", "*.toc", "*.vrb", "*concordance.tex"))
-  unlink(c("*.log", "*.nav", "*.snm", "*.toc", "*.vrb", "*.aux", "*.out"))
-  setwd("V:/My Documents/GitHub/RPM2014/")
+  unlink(c("*.log", "*.nav", "*.snm", "*.toc", "*.vrb", "*.aux", "*.out", "*concordance.tex"))
 }
 
-BuildChild("Part2x")
+BuildChild = function(baseDir, childDir){
+  rnwName = paste0("RPM-", childDir, ".Rnw")
+  texName = paste0("RPM-", childDir, ".tex")
+  childDir = paste0(baseDir, childDir, "/")
+  
+  BuildDoc(childDir, rnwName, texName)
+}
 
+baseDir = "V:/My Documents/GitHub/RPM2014/"
+
+BuildChild(baseDir, "Part1")
+BuildChild(baseDir, "Part2")
+BuildChild(baseDir, "Parent")
+setwd(baseDir)
