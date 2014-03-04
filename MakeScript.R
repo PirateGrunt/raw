de7b02705e628de6635b14ac2713f60f218a9129
BrianFannin <BFannin@RedwoodsGroup.com>
2014-03-04 16:02:53 -0500

    Various

--- MakeScript.R
+++ MakeScript.R
@@ -5,6 +5,7 @@ BuildDoc = function(dir, rnwName, texName){
   setwd(dir)
   knit(rnwName, texName)
   texi2pdf(texName, clean=TRUE, quiet=FALSE)
+  setwd(dir)
   unlink(c("*.log", "*.nav", "*.snm", "*.toc", "*.vrb", "*.aux", "*.out", "*concordance.tex"))
 }
 
@@ -18,8 +19,12 @@ BuildChild = function(baseDir, childDir){
 
 baseDir = "V:/My Documents/GitHub/RPM2014/"
 
-BuildChild(baseDir, "PartGonzo")
 BuildChild(baseDir, "Part1")
-BuildChild(baseDir, "Part2")
+BuildChild(baseDir, "Part2a")
+BuildChild(baseDir, "Part2b")
+BuildChild(baseDir, "Part3a")
+BuildChild(baseDir, "Part3b")
+BuildChild(baseDir, "Part4")
+BuildChild(baseDir, "Part5")
 BuildChild(baseDir, "Parent")
 setwd(baseDir)
