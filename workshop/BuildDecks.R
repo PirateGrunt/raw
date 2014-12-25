38551be065768e9fe1b619b7de7d61b81b9099d0
PirateGrunt <BFannin@RedwoodsGroup.com>
2014-12-25 14:11:05 -0500

    Slide changes

--- workshop/BuildDecks.R
+++ workshop/BuildDecks.R
@@ -16,41 +16,42 @@ slides <- c("Introduction"
             , "LossReserving"
             , "Simulation"
             , "LossDistributions"
-            , "AdvancedModeling"
             , "AdvancedVisualization"
+            , "AdvancedModeling"
             , "Close")
 
 rmdSlides <- paste0("./workshop/", slides, ".Rmd")
 pdfSlides <- gsub(".Rmd", ".pdf", rmdSlides)
 htmlSlides <- gsub(".Rmd", ".html", rmdSlides)
-#htmlSlides <- paste0("./workshop/html/", slides, ".html")
+htmlSlides <- paste0("./workshop/html/", slides, ".html")
 wordSlides <- gsub(".Rmd", ".docx", rmdSlides)
 
-lapply(rmdSlides, render, output_format=slidy_presentation(), output_dir="./html")
+lapply(rmdSlides, render
+       , output_format=slidy_presentation()
+       , output_dir="./html"
+       , envir = new.env())
 if(file.exists("./inst/workshop/html.zip")) unlink("./inst/workshop/html.zip")
-zip("./inst/workshop/html.zip", htmlSlides, flags = "-j9X")
-#file.copy(htmlSlides, "./workshop/html/", overwrite=TRUE)
-unlink(htmlSlides)
+zip("./inst/slides/html.zip", htmlSlides, flags = "-j9X")
 
-lapply(rmdSlides, render, output_format=pdf_document(), output_dir="./pdf")
-if(file.exists("./inst/workshop/pdf.zip")) unlink("./inst/workshop/pdf.zip")
-zip("./inst/workshop/pdf.zip", htmlSlides, flags = "-j9X")
-unlink(pdfSlides)
-
-lapply(rmdSlides, render, output_format=word_document())
-file.copy(wordSlides, "./workshop/word/")
-#zip("./inst/workshop/word.zip", wordSlides, flags = "-9X")
-#file.copy("./inst/workshop/word.zip", "./inst/workshop/")
-unlink(wordSlides)
-
-handouts <- c("A1_FileCommands"
-              , "A2_VectorsAndLists")
-
-handouts <- paste0("./workshop/", handouts, ".Rmd")
-lapply(handouts, render, output_format=pdf_document())
-handouts <- gsub(".Rmd", ".pdf", handouts)
-file.copy(handouts, "./workshop/handouts/")
-oldwd <- setwd("./workshop/handouts/")
-handouts <- gsub("./workshop/", "", handouts)
-zip("handouts.zip", handouts)
-setwd(oldwd)
+# lapply(rmdSlides, render, output_format=pdf_document(), output_dir="./pdf")
+# if(file.exists("./inst/workshop/pdf.zip")) unlink("./inst/workshop/pdf.zip")
+# zip("./inst/workshop/pdf.zip", htmlSlides, flags = "-j9X")
+# unlink(pdfSlides)
+# 
+# lapply(rmdSlides, render, output_format=word_document())
+# file.copy(wordSlides, "./workshop/word/")
+# #zip("./inst/workshop/word.zip", wordSlides, flags = "-9X")
+# #file.copy("./inst/workshop/word.zip", "./inst/workshop/")
+# unlink(wordSlides)
+# 
+# handouts <- c("A1_FileCommands"
+#               , "A2_VectorsAndLists")
+# 
+# handouts <- paste0("./workshop/", handouts, ".Rmd")
+# lapply(handouts, render, output_format=pdf_document())
+# handouts <- gsub(".Rmd", ".pdf", handouts)
+# file.copy(handouts, "./workshop/handouts/")
+# oldwd <- setwd("./workshop/handouts/")
+# handouts <- gsub("./workshop/", "", handouts)
+# zip("handouts.zip", handouts)
+# setwd(oldwd)
