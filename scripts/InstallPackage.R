fd19fb32609f73346f189280ee585ffbf0280bda
PirateGrunt <BFannin@RedwoodsGroup.com>
2015-02-21 21:32:57 -0500

    Fix Dropbox URL.

--- scripts/InstallPackage.R
+++ scripts/InstallPackage.R
@@ -1,14 +1,16 @@
 rawSource <- tempfile()
 MRMR_source <- tempfile()
 
-URL1 <- "https://db.tt/72JO4AE9"
-download.file(URL1, destfile=rawSource, method="curl")
-
-URL2 <- "https://db.tt/8EfsEH9T"
-download.file(URL2, destfile=MRMR_source, method="curl")
-
+URL1 <- "https://dl.dropboxusercontent.com/u/57918578/RPM2015/MRMR_1.0.0.tar.gz"
+download.file(URL1, destfile=MRMR_source, method="curl")
 install.packages(MRMR_source, repos=NULL)
+
+URL2 <- "https://dl.dropboxusercontent.com/u/57918578/RPM2015/raw_0.1.1.tar.gz"
+download.file(URL2, destfile=rawSource, method="curl")
+
 install.packages(rawSource, repos=NULL)
 
 unlink(rawSource)
 unlink(MRMR_source)
+
+rm(URL1, URL2, MRMR_source, rawSource)
