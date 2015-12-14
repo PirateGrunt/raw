6b24d1d3fe9d831586cc9050e51fbdb9a037003c
Fannin <BFannin@redwoodsgroup.com>
2015-12-13 23:39:06 -0500

    More Kevin stuff

--- scripts/04_Data.R
+++ scripts/04_Data.R
@@ -1,287 +1,253 @@
 ## ------------------------------------------------------------------------
-x <- 6
-y <- 6L
-z <- TRUE
-typeof(x)
-typeof(y)
-typeof(z)
-is.logical(x)
-is.double(x)
+A<-matrix(1:6,nrow=2)
+A
 
 ## ------------------------------------------------------------------------
-# Implicit conversion
-w <- TRUE
-x <- 4L
-y <- 5.8
-z <- w + x + y
-typeof(z)
-
-# Explicit conversion. Note loss of data.
-as.integer(z)
+A<-matrix(1:6,nrow=2,byrow=TRUE)
+A
 
 ## ------------------------------------------------------------------------
-class(TRUE)
-class(pi)
-class(4L)
+A<-matrix(1:6,nrow=2,ncol=5)
+A
+
 
 ## ------------------------------------------------------------------------
-class(1:4)
+class(A)
+attributes(A)
+attr(A,"dim")
 
 ## ------------------------------------------------------------------------
-x <- as.Date('2010-01-01')
-class(x)
-typeof(x)
+nrow(A)
+dim(A)[1]
+ncol(A)
+dim(A)[2]
 
-## ----error=TRUE----------------------------------------------------------
-x <- as.Date('06-30-2010')
-
-## ------------------------------------------------------------------------
-x <- as.Date('30-06-2010')
 
 ## ------------------------------------------------------------------------
-x <- as.Date('2010-06-30')
+A[2,2]
+A[1,]
+A[,2]
 
 ## ------------------------------------------------------------------------
-x <- Sys.Date()
-y <- Sys.time()
+class(A[1,])
+class(A[1,,drop=FALSE])
+A[1,,drop=FALSE]
 
 ## ------------------------------------------------------------------------
-myColors <- c("Red", "Blue", "Green", "Red", "Blue", "Red")
-myFactor <- factor(myColors)
-typeof(myFactor)
-class(myFactor)
-is.character(myFactor)
-is.character(myColors)
+set.seed(6345789)
+x<-sample(1:20,size=9,replace=TRUE)
+m1<-matrix(x,nrow=3)
+m1
 
 ## ------------------------------------------------------------------------
-# This probably won't give you what you expect
-myOtherFactor <- c(myFactor, "Orange")
-myOtherFactor
-
-# And this will give you an error
-myFactor[length(myFactor)+1] <- "Orange"
-
-# Must do things in two steps
-myOtherFactor <- factor(c(levels(myFactor), "Orange"))
-myOtherFactor[length(myOtherFactor)+1] <- "Orange"
+m1[c(1,3),c(2,3)]
 
 ## ------------------------------------------------------------------------
-myLogical <- TRUE
-myInteger <- 1:4
-myDouble <- 3.14
-myCharacter <- "Hello!"
-
-y <- myLogical + myInteger
-typeof(y)
-y <- myInteger + myDouble
-typeof(y)
+m1[-2,-1]
 
 ## ------------------------------------------------------------------------
-myVector <- 1:100
-myMatrix <- matrix(myVector, nrow=10, ncol=10)
-
-myOtherMatrix <- myVector
-dim(myOtherMatrix) <- c(10,10)
-
-identical(myMatrix, myOtherMatrix)
+rownames(m1)
+colnames(m1)
+rownames(m1)<-c("row1","row2","row3")
+colnames(m1)<-c("col1","col2","col3")
+m1[c("row1","row2"),c("col2","col3")]
 
 ## ------------------------------------------------------------------------
-myMatrix <- matrix(nrow=10, ncol=10)
 
-## ----echo=FALSE----------------------------------------------------------
-library(rblocks)
-block_grid(10, 10, type="matrix")
-
-## ------------------------------------------------------------------------
-dim(myMatrix) <- c(25, 4)
-
-## ----echo=FALSE----------------------------------------------------------
-block_grid(25, 4, type="matrix")
+B<-matrix(c("a","b","c","d"),nrow=2);B
+C<-list(1,2,3,"a")
+dim(C)<-c(2,2);C
 
 ## ------------------------------------------------------------------------
-myMatrix <- matrix(nrow=10, ncol=10, data = sample(1:100))
-colnames(myMatrix) <- letters[1:10]
-head(myMatrix, 3)
-rownames(myMatrix) <- tail(letters, 10)
-head(myMatrix, 3)
+A<-matrix(1:6,nrow=2)
+B<-rbind(A,7:9)
+B
+dim(B)
 
 ## ------------------------------------------------------------------------
-myMatrix[2, ]
-myMatrix[, 2]
+C<-cbind(A,10:11)
+C
+dim(C)
 
 ## ------------------------------------------------------------------------
-myMatrix[2]
-myMatrix[22]
+A<-matrix(1:6,nrow=2)
+3*A
+A+A
 
 ## ------------------------------------------------------------------------
-sum(myMatrix)
-colSums(myMatrix)
-rowSums(myMatrix)
-colMeans(myMatrix)
+A<-matrix(1:6,nrow=2)
+A^2
+t(A)
 
 ## ------------------------------------------------------------------------
-x <- list()
-typeof(x)
-x[[1]] <- c("Hello", "there", "this", "is", "a", "list")
-x[[2]] <- c(pi, exp(1))
-summary(x)
-str(x)
-
-## ----echo=FALSE----------------------------------------------------------
-make_block(x)
+B<-matrix(c(-1,6,8,7,4,1),nrow=3)
+C<-A %*% B
+dim(C)
+C
+# B %*% A will give an error
 
 ## ------------------------------------------------------------------------
-y <- list()
-y[[1]] <- "Lou Reed"
-y[[2]] <- 45
-
-x[[3]] <- y
-
-## ----echo=FALSE----------------------------------------------------------
-make_block(x)
+(A<-matrix(c(1,1,3,2),nrow=2,byrow=TRUE))
+(B<-matrix(c(8,18),nrow=2))
 
 ## ------------------------------------------------------------------------
-y[[1]] <- c("Lou Reed", "Patti Smith")
-y[[2]] <- c(45, 63)
-
-names(y) <- c("Artist", "Age")
-
-y$Artist
-y$Age
+det(A)
 
 ## ------------------------------------------------------------------------
-myList <- list(firstVector = c(1:10)
-               , secondVector = c(89, 56, 84, 298, 56)
-               , thirdVector = c(7,3,5,6,2,4,2))
-lapply(myList, mean)
-lapply(myList, median)
-lapply(myList, sum)
+solve(A,B)
 
 ## ------------------------------------------------------------------------
-set.seed(1234)
-State = rep(c("TX", "NY", "CA"), 10)
-EarnedPremium = rlnorm(length(State), meanlog = log(50000), sdlog=1)
-EarnedPremium = round(EarnedPremium, -3)
-Losses = EarnedPremium * runif(length(EarnedPremium), min=0.4, max = 0.9)
-
-df = data.frame(State, EarnedPremium, Losses, stringsAsFactors=FALSE)
+solve(A)
+solve(A)%*%B
 
 ## ------------------------------------------------------------------------
-summary(df)
-str(df)
+eigen(A)
 
 ## ------------------------------------------------------------------------
-names(df)
-colnames(df)
-length(df)
-dim(df)
-nrow(df)
-ncol(df)
+diag(A)
+diag(c(2,4,6,8))
 
 ## ------------------------------------------------------------------------
-head(df)
-head(df, 2)
-tail(df)
+(A<-matrix(1:6,nrow=2))
+rowSums(A)
+colSums(A)
+
+## ------------------------------------------------------------------------
+rowMeans(A)
+colMeans(A)
+
+## ------------------------------------------------------------------------
+teams<-c("Toronto","New York","Baltimore","Tampa Bay","Boston")
+wins<-c(93,87,81,80,78)
+losses<-c(69,75,81,82,84)
+
+## ------------------------------------------------------------------------
+alEast<-matrix(c(wins,losses),nrow=5,byrow=FALSE)
+alEast
+
+## ------------------------------------------------------------------------
+rownames(alEast)<-teams
+colnames(alEast)<-c("Wins","Losses")
+alEast
+
+## ------------------------------------------------------------------------
+Pct<-round(alEast[,"Wins"]/(alEast[,"Wins"]+alEast[,"Losses"]),3)
+alEast<-cbind(alEast,Pct)
+alEast
+
+## ------------------------------------------------------------------------
+alEast<-matrix(c(teams,wins,losses),nrow=5,byrow=FALSE)
+alEast
+
+## ------------------------------------------------------------------------
+class(alEast[,2])
+as.numeric(alEast[,2])
+alEast[,2]<-as.numeric(alEast[,2])
+alEast
+
+## ------------------------------------------------------------------------
+alEast<-cbind(teams,wins,losses)
+alEast
+class(alEast)
+
+## ------------------------------------------------------------------------
+alEast<-data.frame(Team=teams,W=wins,L=losses)
+alEast
+class(alEast)
+
+
+## ------------------------------------------------------------------------
+sapply(alEast,class)
+
+## ------------------------------------------------------------------------
+alEast<-data.frame(Team=teams,W=wins,L=losses,
+                   stringsAsFactors=FALSE)
+sapply(alEast,class)
+
+## ------------------------------------------------------------------------
+alEast[,"Team",drop=FALSE]
+class(alEast[,"Team",drop=FALSE])
+
+## ------------------------------------------------------------------------
+alEast$W
+
+alEast[[2]]
+
+dim(alEast)
+
+## ------------------------------------------------------------------------
+attributes(alEast)
+
+## ------------------------------------------------------------------------
+library(Lahman)
+class(Teams)
+dim(Teams)
+
+## ------------------------------------------------------------------------
+names(Teams)
+
+## ------------------------------------------------------------------------
+head(row.names(Teams))
+nrow(Teams)
+ncol(Teams)
+
+## ------------------------------------------------------------------------
+head(Teams,6)
+
+## ------------------------------------------------------------------------
+tail(Teams,4)
+
+## ------------------------------------------------------------------------
+summary(Teams)
+
+## ------------------------------------------------------------------------
+str(Teams)
+
+## ------------------------------------------------------------------------
+myTeams<-Teams[,c("yearID","lgID","teamID","divID","W","L")]
+head(myTeams)
+
+## ------------------------------------------------------------------------
+str(myTeams)
+unique(myTeams$divID)
+
+## ------------------------------------------------------------------------
+alEast2014<-myTeams[myTeams$yearID==2014 & myTeams$lgID=="AL" 
+                    & myTeams$divID=="E",]
+alEast2014
+
+## ------------------------------------------------------------------------
+alEast2014<-alEast2014[,c("teamID","W","L")]
+names(alEast2014)<-c("Team","Wins","Losses")
+alEast2014
+
+## ------------------------------------------------------------------------
+alEast2014$Pct<-round(alEast2014$Wins/(alEast2014$Wins+alEast2014$Losses),3)
+alEast2014
+
+## ------------------------------------------------------------------------
+alEast2014[order(-alEast2014$Pct),]
+
+## ------------------------------------------------------------------------
+mlb2014<-subset(Teams,yearID==2014)
+mlb2014<-mlb2014[,c("lgID","divID","W")]
+d1<-aggregate(W~lgID+divID,data=mlb2014,sum)
 
 ## ----eval=FALSE----------------------------------------------------------
-## df[2,3]
-## df[2]
-## df[2,]
-## df[2, -1]
+## x<-data.frame()
+## x<-edit(x)
 
-## ----eval=FALSE----------------------------------------------------------
-## df$EarnedPremium
-## # Columns of a data frame may be treated as vectors
-## df$EarnedPremium[3]
-## df[2:4, 1:2]
-## df[, "EarnedPremium"]
-## df[, c("EarnedPremium", "State")]
+## ----echo=TRUE,eval=FALSE------------------------------------------------
+## library(readxl)
+## f1<-file.choose() #opens dialog box
+## d1<-excel_sheets(f1)
+## class(d1)
+## d2<-read_excel(f1) #default is first sheet
 
 ## ------------------------------------------------------------------------
-order(df$EarnedPremium)
-df = df[order(df$EarnedPremium), ]
+suppressWarnings(library(readxl))
+datasets <- system.file("extdata/datasets.xlsx", package = "readxl")
+excel_sheets(datasets)
+head(read_excel(datasets,sheet="mtcars"))
 
-## ------------------------------------------------------------------------
-df$LossRatio = df$EarnedPremium / df$Losses
-df$LossRatio = 1 / df$LossRatio
-
-## ------------------------------------------------------------------------
-df$LossRatio = NULL
-df = df[, 1:2]
-
-## ----results='hide'------------------------------------------------------
-dfA = df[1:10,]
-dfB = df[11:20, ]
-rbind(dfA, dfB)
-dfC = dfA[, 1:2]
-cbind(dfA, dfC)
-
-## ----size='tiny'---------------------------------------------------------
-dfRateChange = data.frame(State =c("TX", "CA", "NY"), RateChange = c(.05, -.1, .2))
-df = merge(df, dfRateChange)
-
-## ------------------------------------------------------------------------
-df$LossRation = with(df, Losses / EarnedPremium)
-names(df)
-colnames(df)[4] = "Loss Ratio"
-colnames(df)
-
-## ------------------------------------------------------------------------
-dfTX = subset(df, State == "TX")
-dfBigPolicies = subset(df, EarnedPremium >= 50000)
-
-## ------------------------------------------------------------------------
-dfTX = df[df$State == "TX", ]
-dfBigPolicies = df[df$EarnedPremium >= 50000, ]
-
-## ------------------------------------------------------------------------
-whichState = df$State == "TX"
-dfTX = df[whichState, ]
-
-whichEP = df$EarnedPremium >= 50000
-dfBigPolicies = df[whichEP, ]
-
-## ------------------------------------------------------------------------
-sum(df$EarnedPremium)
-sum(df$EarnedPremium[df$State == "TX"])
-
-aggregate(df[,-1], list(df$State), sum)
-
-## ----size='tiny', fig.height=5-------------------------------------------
-dfByState = aggregate(df$EarnedPremium, list(df$State), sum)
-colnames(dfByState) = c("State", "EarnedPremium")
-barplot(dfByState$EarnedPremium, names.arg=dfByState$State, col="blue")
-
-## ----size='tiny', fig.height=5-------------------------------------------
-dotchart(dfByState$EarnedPremium, dfByState$State, pch=19)
-
-## ----eval=FALSE----------------------------------------------------------
-## myData = read.csv("SomeFile.csv")
-
-## ----eval=FALSE----------------------------------------------------------
-## library(XLConnect)
-## wbk = loadWorkbook("myWorkbook.xlsx")
-## df = readWorksheet(wbk, someSheet)
-
-## ----eval=FALSE----------------------------------------------------------
-## URL = "http://www.casact.org/research/reserve_data/ppauto_pos.csv"
-## df = read.csv(URL, stringsAsFactors = FALSE)
-
-## ----eval=FALSE----------------------------------------------------------
-## library(XML)
-## URL = "http://www.pro-football-reference.com/teams/nyj/2012_games.htm"
-## games = readHTMLTable(URL, stringsAsFactors = FALSE)
-
-## ----eval=FALSE----------------------------------------------------------
-## library(RODBC)
-## myChannel = odbcConnect(dsn = "MyDSN_Name")
-## df = sqlQuery(myChannel, "SELECT stuff FROM myTable")
-
-## ----eval=FALSE----------------------------------------------------------
-## df = read.csv("../data/StateData.csv")
-
-## ----eval=FALSE----------------------------------------------------------
-## View(df)
-
-## ------------------------------------------------------------------------
 
