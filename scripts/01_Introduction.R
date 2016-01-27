c4592674ac9919d4d049fb6f1505fcf9c1c97c77
Brian Fannin <FanninQED@Yahoo.com>
2016-01-26 22:02:14 -0500

    Remove other authors content for RPM

--- scripts/01_Introduction.R
+++
@@ -1,3 +0,0 @@
-## ----eval=FALSE, echo=FALSE----------------------------------------------
-## ![I hope theres an image here](images/R.jpg)
-
--- scripts/VectorQuestion.R
+++
@@ -1,7 +0,0 @@
-FirstName <- c("Richard", "James", "Ronald", "Ronald"
-               , "George", "William", "William", "George"
-               , "George", "Barack", "Barack")
-LastName <- c("Nixon", "Carter", "Reagan", "Reagan"
-              , "Bush", "Clinton", "Clinton", "Bush"
-              , "Bush", "Obama", "Obama")
-ElectionYear <- seq(1972, 2012, 4)
